73d154a733d070585039459e31a0572fea4acfc5
Vijendar Komalla <vikomall@users.noreply.github.com>
2016-04-19 13:41:37 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ pip help
 [[ $? != 0 ]] && echo python-pip is not installed. && exit 1
 
 # Install dependencies from app requirements file
-cd /app/proxy
+#cd /app/proxy
 #pip install -r requirements.txt
